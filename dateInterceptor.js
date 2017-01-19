7e4753e49fe9d04c4f6f30670fe57556d4dce11d
ChangYinShung <changyenshung@gmail.com>
2017-01-19 10:15:26 +0800

    修改ISO UTC
    2000 被視為日期格式

--- dateInterceptor.js
+++ dateInterceptor.js
@@ -1,6 +1,6 @@
 ﻿(function (app) {
     app.factory('dateInterceptor', function () {
-            var regexIsoUtc = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+            var regexIsoUtc = /(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})\+(\d{2})\:(\d{2})/;
 
             function matchDate(dateString) {
                 return dateString.match(regexIsoUtc);
